b01de4914914fe125d52db5c73bd174d23eac978
Rully Ramanda <krooco@gmail.com>
2020-01-20 10:45:39 +0700

    Blank template ready

---
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DefaultFooter = () => (
+  <>
+    <span><a href="https://coreui.io">CoreUI</a> &copy; 2019 creativeLabs.</span>
+    <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+  </>
+);
+
+export default DefaultFooter;
---
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
+
+import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import logo from '../../assets/img/brand/logo.svg';
+import sygnet from '../../assets/img/brand/sygnet.svg';
+
+const DefaultHeader = () => (
+  <>
+    <AppSidebarToggler className="d-lg-none" display="md" mobile />
+    <AppNavbarBrand
+      full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+      minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+    />
+    <AppSidebarToggler className="d-md-down-none" display="lg" />
+
+    <Nav className="ml-auto" navbar>
+      <NavItem className="d-md-down-none">
+        <NavLink to="#" className="nav-link"><i className="icon-bell"></i></NavLink>
+      </NavItem>
+      <UncontrolledDropdown nav direction="down">
+        <DropdownToggle nav>
+          <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+        </DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
+          <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
+          <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
+          <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  </>
+);
+
+export default DefaultHeader;
