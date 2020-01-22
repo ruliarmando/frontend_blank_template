06a5314dc29f031cfd5e93821592fe3acf6f89c3
Rully Ramanda <krooco@gmail.com>
2020-01-22 09:54:28 +0700

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import './App.scss';
 
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 
-// Containers
 const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
 
-// Pages
 const Login = React.lazy(() => import('./views/Pages/Login'));
 const Register = React.lazy(() => import('./views/Pages/Register'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
